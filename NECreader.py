d9f30129051825da907fc67a01617e202a4399a7
Tomas Zikmund <tommekster@gmail.com>
2016-10-01 03:35:42 +0200

    It gives several time intervals. This shut be decoded to 0 and 1.

--- NECreader.py
+++ NECreader.py
@@ -5,11 +5,33 @@ try:
 except RuntimeError:
 	print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 import time
+#import threading.Timer as Timer
+#from threading import Timer
 
-begin_time = time.time()
+lastTime = time.time()
+newEvent = False
+times = list()
+
+def decodeRC():
+	global newEvent
+	newEvent = False
+	print("Button has been pressed, length of list of times: " + str(len(times)))
+	if len(times) == 0:
+		return
+	intervals = [(u-v) for u,v in zip(times[1:],times)]
+	print("Intervals:")
+	for i in intervals:
+		print(str(i))
+	print(" ")
+	del times[:]
 
 def myCallback(pin):
-	print("event on pin " + str(pin) + " at " + str(time.time()-begin_time))
+	global newEvent
+	global lastTime
+	lastTime = time.time()
+	times.append(time.time())
+	newEvent = True
+	#print("Callback")
 
 if __name__ == '__main__':
 	GPIO.setmode(GPIO.BOARD)
@@ -17,7 +39,9 @@ if __name__ == '__main__':
 	GPIO.add_event_detect(channel, GPIO.FALLING, callback=myCallback)
 	try:
 		while True:
-			time.sleep(1)
+			time.sleep(0.5)
+			if newEvent and time.time() > (lastTime + 0.03): 
+				decodeRC()
 	except KeyboardInterrupt:
 		pass
 	GPIO.remove_event_detect(channel)
