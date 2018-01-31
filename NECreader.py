2d7f1d72a389e88497e1b5dcbd8272b457801fb2
Tomas Zikmund <tommekster@gmail.com>
2018-02-01 00:25:38 +0100

    uprava RCservice pres socket, udrzeni spojeni

--- NECreader.py
+++ NECreader.py
@@ -5,14 +5,14 @@ try:
 except RuntimeError:
 	print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 import time
-#import threading.Timer as Timer
-#from threading import Timer
-
 # https://docs.python.org/2/library/signal.html
+
+
 
 lastTime = time.time()
 newEvent = False
 times = list()
+
 
 def list2num(l):
 	b = 1
@@ -22,11 +22,15 @@ def list2num(l):
 		b = 2*b
 	return n
 
+
 def printResult(a,c):
 	print(a,c)
 
+
 def decodeRC(callback = printResult):
 	global newEvent
+	global times
+
 	newEvent = False
 	if len(times) < 34:
 		return
@@ -37,30 +41,41 @@ def decodeRC(callback = printResult):
 	callback(address, command)	
 	del times[:]
 
+
 def myCallback(pin):
 	global newEvent
 	global lastTime
+	global times
+
 	lastTime = time.time()
 	times.append(time.time())
 	newEvent = True
+
 
 def setup(pin):
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.add_event_detect(pin, GPIO.FALLING, callback=myCallback)
 
+
 def cleanup(pin):
 	GPIO.remove_event_detect(pin)
 	GPIO.cleanup()
 
+
 def callIfEvent(callback):
+	global newEvent
+	global lastTime
+
 	if newEvent and time.time() > (lastTime + 0.03): 
 		decodeRC(callback)
+
 
 def poolRC(callback = printResult):
 	while True:
 		time.sleep(0.5)
 		callIfEvent(callback)
+
 
 if __name__ == '__main__':
 	setup(channel)
--- RCservice.py
+++ RCservice.py
@@ -1,43 +1,48 @@
 #! /usr/bin/env python
 import NECreader
 import socket
+import threading
+import time
 
 HOST = '127.0.0.1'
 PORT = 50007
+DEBUG = True
 
 class RCservice:
 	def __init__(s,host,port):
 		s.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.socket.bind((host,port))
-		s.socket.listen(1)
-		s.socket.settimeout(0.5)
-		s.history = []
+		s.socket.listen(5)
+		s.clients = []
+		s.running = True
 		NECreader.setup(NECreader.channel)
+		def poolRC():
+			while s.running:
+				time.sleep(0.5)
+				NECreader.callIfEvent(s.rcEvent)
+		threading.Thread(target=poolRC).start()
 		
 	def rcEvent(s,address,command):
-		s.history.append((address,command))
-		
-	def socketEvent(s,conn,addr):
-		if len(s.history) == 0:
-			conn.sendall("empty")
-		else:
-			button = s.history[-1]
-			conn.sendall(str(button[0]) + " " + str(button[1]))
-			s.history = []
-		conn.close()
-		
+		if DEBUG: print("rcEvent "+str((address,command)))
+		if len(s.clients) == 0: return
+		disconnected = []
+		if DEBUG: print("clients "+str(len(s.clients)))
+		for c in s.clients:
+			msg = str((address,command))+"\r\n"
+			try: c.sendall(msg.encode('utf-8'))
+			except: disconnected.append(c)
+		s.clients = [c for c in s.clients if not c in disconnected]
+
 	def listen(s):
+		if not s.running: return
 		while True:
-			timeout = False
-			try:
-				conn, addr = s.socket.accept()
-			except socket.timeout:
-				timeout = True
-			if not timeout:
-				s.socketEvent(conn,addr)
-			NECreader.callIfEvent(s.rcEvent)
+			conn, addr = s.socket.accept()
+			s.clients.append(conn)
+			if DEBUG: print("new connection "+str(addr))
 		
 	def cleanup(s):
+		s.running = False
+		s.socket.close()
 		NECreader.cleanup(NECreader.channel)
 		
 if __name__ == '__main__':
